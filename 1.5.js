9585e3b19dcdf040d39b6ea7764c2e373db1ef2a
HanLoi <hannecartloic@gmail.com>
2021-01-18 16:08:35 +0100

    1.5 modif

--- 1.5.js
+++ 1.5.js
@@ -1,3 +1,4 @@
 const readlineSync = require("readline-sync");
 let number = readlineSync.question("Can you tell me a number with a quote ?");
-console.log(Math.round(number));
+let a = number * 2;
+console.log(Math.round(a));
