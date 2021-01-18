1294eff135fc4b368bebf6db529b5702d51b4ffc
HanLoi <hannecartloic@gmail.com>
2021-01-18 15:05:35 +0100

    1.4

---
+++ 1.4.js
@@ -0,0 +1,5 @@
+const readlineSync = require("readline-sync");
+let name = readlineSync.question("What is your name ?");
+let firstname = readlineSync.question("What is your first name ?");
+let city = readlineSync.question("What is your city ?");
+console.log("Your name is " + name + firstname+ "and you live in " + city)
