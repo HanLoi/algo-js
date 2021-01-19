027182812ac7d3451f2b8b98e5797b98eeed1c2b
HanLoi <hannecartloic@gmail.com>
2021-01-19 14:14:24 +0100

    modif 1.5

--- 1.5.js
+++ 1.5.js
@@ -1,4 +1,4 @@
 const readlineSync = require("readline-sync");
-let number = readlineSync.question("Can you tell me a number with a quote ?");
+let number = readlineSync.question("Can you tell me a number with decimal ?");
 let a = number * 2;
-console.log(Math.round(a));
+console.log(Math.trunc(a));
