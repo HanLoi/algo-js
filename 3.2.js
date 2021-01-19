41dccef445e2edf28e0602ab58f63c15e8e1d57e
HanLoi <hannecartloic@gmail.com>
2021-01-19 10:39:41 +0100

    3.2

---
+++ 3.2.js
@@ -0,0 +1,15 @@
+let arr = [1,2,3,4,5];
+let arrl = arr.length
+var som = new Number(0);
+arr.forEach(element => {
+    som = som+element;
+});
+console.log(som/arrl);
+
+let arr2 = [100,101,102];
+let arr2l = arr2.length
+var som2 = new Number(0);
+arr2.forEach(element => {
+    som2 = som2+element;
+});
+console.log(som2/arr2l);
