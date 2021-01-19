e6f99fb47169989a4ce4c8de62b6d71c1ad9fda0
HanLoi <hannecartloic@gmail.com>
2021-01-19 09:09:24 +0100

    2.6

---
+++ 2.6.js
@@ -0,0 +1,19 @@
+const readlineSync = require("readline-sync");
+let number = readlineSync.question("Tell me a number between 1 at 7 ");
+
+if (number = 1) {
+    console.log("It's monday")
+}else if (number = 2){
+    console.log("It's tuesday")
+} else if (number = 3){
+    console.log("It's wednesday")
+}else if (number = 4){
+    console.log("It's thursday")
+}else if (number = 5){
+    console.log("It's friday")
+}else if (number = 6){
+    console.log("It's saturday")
+}else if (number = 7){
+    console.log("It's sunday")
+}
+
