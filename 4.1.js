8dc275b285f16c2a36ce8e48dfa48db7b3ddd84f
HanLoi <hannecartloic@gmail.com>
2021-01-19 14:12:20 +0100

    4.1

--- 4.1.js
+++ 4.1.js
@@ -7,3 +7,5 @@ var c = Math.floor (readlineSync.question("What's the width of your rectangle ? 
 var d = Math.floor (readlineSync.question("What's the height of your rectangle ? "))
 
 console.log("The surface of your rectangle is " + calcSurface (c, d) + " m²")
+
+/** calcSurface is a function for calculate the surface of a rectangle */
