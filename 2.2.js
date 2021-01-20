ae71bf9120b5c1379dbec98560b8d1acfad8bb69
HanLoi <hannecartloic@gmail.com>
2021-01-20 09:51:38 +0100

    modif 2.2

--- 2.2.js
+++ 2.2.js
@@ -1,10 +1,10 @@
 const readlineSync = require("readline-sync");
-let min = readlineSync.question("Give me a number that will be your minimum number");
-let current = readlineSync.question("Give me a number that will be your current number");
-let max = readlineSync.question("Give me a number that will be your maximum number");
+let min = readlineSync.question("Give me a number that will be your minimum number ");
+let current = readlineSync.question("Give me a number that will be your current number ");
+let max = readlineSync.question("Give me a number that will be your maximum number ");
 
 if ((current < max ) && (current > min)) {
-    console.log(current)
+    console.log("Your current number is " + current)
 } else if (min > max) {
     console.log("BIIIIIIIIIPPPP !!! WRONNNG !!! you put a maximum who is lower than your minimum number !! Go return in primary school guy !!")
 }
